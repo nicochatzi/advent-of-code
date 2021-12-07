fd5b6a27117355993aaae650cc0ab708d37899ef
Nico Chatzi <nico.chatzigianis@focusrite.com>
2021-12-07 08:26:29 +0000

    star 13

--- 2021/day-07/src/main.rs
+++ 2021/day-07/src/main.rs
@@ -1,12 +1,20 @@
-fn find_lsd(values: &[isize]) -> isize {
+fn find_lsd_with_func(values: &[isize], func: impl Fn(isize, isize) -> isize) -> isize {
     let max = *values.iter().max().unwrap();
     let min = *values.iter().min().unwrap();
     (min..=max).fold(isize::MAX, |lsd, n| {
         values
             .iter()
-            .fold(0, |diff_sum, x| diff_sum + (x - n).abs())
+            .fold(0, |diff_sum, x| diff_sum + func(*x, n))
             .min(lsd)
     })
+}
+
+fn find_lsd(values: &[isize]) -> isize {
+    find_lsd_with_func(values, |a, b| (a - b).abs())
+}
+
+fn find_lsd_with_weighted_step(values: &[isize]) -> isize {
+    find_lsd_with_func(values, |a, b| (1..=(a - b).abs()).sum::<isize>())
 }
 
 fn parse_input(s: &str) -> Vec<isize> {
@@ -18,6 +26,10 @@ fn main() {
         "star 12 : {}",
         find_lsd(&parse_input(include_str!("../res/data.txt")))
     );
+    println!(
+        "star 13 : {}",
+        find_lsd_with_weighted_step(&parse_input(include_str!("../res/data.txt")))
+    );
 }
 
 #[cfg(test)]
@@ -28,4 +40,9 @@ fn aoc_input() -> Vec<isize> {
 #[test]
 fn can_find_aoc_input_result_star_12() {
     assert_eq!(find_lsd(&aoc_input()), 37);
+}
+
+#[test]
+fn can_find_aoc_input_result_star_13() {
+    assert_eq!(find_lsd_with_weighted_step(&aoc_input()), 168);
 }
