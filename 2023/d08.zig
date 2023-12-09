a625d5b45199dfe0bd90f64a963817addc8714a7
Nico Chatzi <nico.chatzigianis@focusrite.com>
2023-12-09 13:12:00 +0000

    aoc23: d08 - 2

--- 2023/d08.zig
+++ 2023/d08.zig
@@ -20,12 +20,58 @@ fn walkToTerminal(network: *const Network, instructions: *const Instructions) us
     return step;
 }
 
-fn partOne(sequence: []const u8) !usize {
-    var instructions = Instructions.init(std.heap.page_allocator);
-    defer instructions.deinit();
+fn walkToZ(network: *const Network, instructions: *const Instructions, start: Node) usize {
+    var node = start;
+    var step: usize = 0;
+    while (node[2] != 'Z') : (step += 1) {
+        const map = network.get(node) orelse unreachable;
+        switch (instructions.items[step % instructions.items.len]) {
+            .l => node = map.l,
+            .r => node = map.r,
+        }
+    }
+    return step;
+}
 
+fn gcm(a: usize, b: usize) usize {
+    var a_ = a;
+    var b_ = b;
+    while (b_ != 0) {
+        const temp = b_;
+        b_ = a_ % b_;
+        a_ = temp;
+    }
+    return a_;
+}
+
+fn resolvePartTwo(network: *const Network, instructions: *const Instructions) usize {
+    var starts = std.ArrayList(Node).init(std.heap.page_allocator);
+    defer starts.deinit();
+
+    var it = network.iterator();
+    while (it.next()) |entry| {
+        if (entry.key_ptr.*[2] == 'A') {
+            starts.append(entry.key_ptr.*) catch unreachable;
+        }
+    }
+
+    var steps = std.ArrayList(usize).init(std.heap.page_allocator);
+    defer steps.deinit();
+
+    for (starts.items) |start|
+        steps.append(walkToZ(network, instructions, start)) catch unreachable;
+
+    var result: usize = 1;
+    for (steps.items) |step| result = result * step / gcm(result, step);
+    return result;
+}
+
+fn run(comptime solver: fn (*const Network, *const Instructions) usize, sequence: []const u8) !usize {
     var network = Network.init(std.heap.page_allocator);
     defer network.deinit();
+
+    var instructions = Instructions.init(std.heap.page_allocator);
+    defer instructions.deinit();
 
     var it = std.mem.tokenizeSequence(u8, sequence, "\n");
     for (it.next().?) |c| {
@@ -44,7 +90,15 @@ fn partOne(sequence: []const u8) !usize {
         try network.put(entry.node, entry.map);
     }
 
-    return walkToTerminal(&network, &instructions);
+    return solver(&network, &instructions);
+}
+
+fn partOne(sequence: []const u8) !usize {
+    return try run(walkToTerminal, sequence);
+}
+
+fn partTwo(sequence: []const u8) !usize {
+    return try run(resolvePartTwo, sequence);
 }
 
 pub fn main() !void {
@@ -53,6 +107,10 @@ pub fn main() !void {
 
     std.debug.print("one : {d}\ntime: {}\n\n", .{
         try partOne(data),
+        std.fmt.fmtDuration(timer.lap()),
+    });
+    std.debug.print("two : {d}\ntime: {}\n\n", .{
+        try partTwo(data),
         std.fmt.fmtDuration(timer.lap()),
     });
 }
@@ -64,5 +122,20 @@ test partOne {
         \\AAA = (BBB, BBB)
         \\BBB = (AAA, ZZZ)
         \\ZZZ = (ZZZ, ZZZ)
+    ), 6);
+}
+
+test partTwo {
+    try std.testing.expectEqual(try partTwo(
+        \\LR
+        \\
+        \\11A = (11B, XXX)
+        \\11B = (XXX, 11Z)
+        \\11Z = (11B, XXX)
+        \\22A = (22B, XXX)
+        \\22B = (22C, 22C)
+        \\22C = (22Z, 22Z)
+        \\22Z = (22B, 22B)
+        \\XXX = (XXX, XXX)
     ), 6);
 }
