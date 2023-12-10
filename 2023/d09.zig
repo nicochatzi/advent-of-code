6911d5dc21c01a2a48e0dc986310bedbde5fabc4
Nico Chatzi <nico.chatzigianis@focusrite.com>
2023-12-10 10:48:47 +0000

    aoc23: d09 - 2

--- 2023/d09.zig
+++ 2023/d09.zig
@@ -5,7 +5,7 @@ fn all_zero(sequence: []const isize) bool {
     return true;
 }
 
-fn diff(sequence: []const isize) !std.ArrayList(isize) {
+fn build_diff(sequence: []const isize) !std.ArrayList(isize) {
     var result = std.ArrayList(isize).init(std.heap.page_allocator);
     var i: usize = 0;
     while (i < sequence.len - 1) : (i += 1)
@@ -13,15 +13,21 @@ fn diff(sequence: []const isize) !std.ArrayList(isize) {
     return result;
 }
 
-fn extrapolate(sequence: []const isize) !isize {
-    var diffs = std.ArrayList(std.ArrayList(isize)).init(std.heap.page_allocator);
+fn build_diffs(sequence: []const isize) !std.ArrayList(std.ArrayList(isize)) {
+    var stack = std.ArrayList(std.ArrayList(isize)).init(std.heap.page_allocator);
     var first = std.ArrayList(isize).init(std.heap.page_allocator);
-    for (sequence) |v| try first.append(v);
-    try diffs.append(first);
-    while (!all_zero(diffs.getLast().items))
-        try diffs.append(try diff(diffs.getLast().items));
+    try first.appendSlice(sequence);
+    try stack.append(first);
 
-    try diffs.items[diffs.items.len - 1].append(0);
+    while (!all_zero(stack.getLast().items))
+        try stack.append(try build_diff(stack.getLast().items));
+
+    try stack.items[stack.items.len - 1].append(0);
+    return stack;
+}
+
+fn extrapolate1(sequence: []const isize) !isize {
+    var diffs = try build_diffs(sequence);
 
     const len: isize = @intCast(diffs.items.len);
     var i: isize = len - 2;
@@ -34,6 +40,22 @@ fn extrapolate(sequence: []const isize) !isize {
     }
 
     return diffs.items[0].getLast();
+}
+
+fn extrapolate2(sequence: []const isize) !isize {
+    var diffs = try build_diffs(sequence);
+
+    const len: isize = @intCast(diffs.items.len);
+    var i: isize = len - 2;
+    while (i >= 0) : (i -= 1) {
+        const i_: usize = @intCast(i);
+        var curr = &diffs.items[i_];
+        var next = &diffs.items[i_ + 1];
+        const step = curr.items[0] - next.items[0];
+        try curr.insert(0, step);
+    }
+
+    return diffs.items[0].items[0];
 }
 
 fn parse(sequence: []const u8) !std.ArrayList(std.ArrayList(isize)) {
@@ -52,7 +74,14 @@ fn parse(sequence: []const u8) !std.ArrayList(std.ArrayList(isize)) {
 fn partOne(sequence: []const u8) !isize {
     const lists = try parse(sequence);
     var result: isize = 0;
-    for (lists.items) |list| result += try extrapolate(list.items);
+    for (lists.items) |list| result += try extrapolate1(list.items);
+    return result;
+}
+
+fn partTwo(sequence: []const u8) !isize {
+    const lists = try parse(sequence);
+    var result: isize = 0;
+    for (lists.items) |list| result += try extrapolate2(list.items);
     return result;
 }
 
@@ -64,12 +93,16 @@ pub fn main() !void {
         try partOne(data),
         std.fmt.fmtDuration(timer.lap()),
     });
+    std.debug.print("two : {d}\ntime: {}\n\n", .{
+        try partTwo(data),
+        std.fmt.fmtDuration(timer.lap()),
+    });
 }
 
-test extrapolate {
-    try std.testing.expectEqual(extrapolate(&[_]isize{ 0, 3, 6, 9, 12, 15 }), 18);
-    try std.testing.expectEqual(extrapolate(&[_]isize{ 1, 3, 6, 10, 15, 21 }), 28);
-    try std.testing.expectEqual(extrapolate(&[_]isize{ 10, 13, 16, 21, 30, 45 }), 68);
+test extrapolate1 {
+    try std.testing.expectEqual(extrapolate1(&[_]isize{ 0, 3, 6, 9, 12, 15 }), 18);
+    try std.testing.expectEqual(extrapolate1(&[_]isize{ 1, 3, 6, 10, 15, 21 }), 28);
+    try std.testing.expectEqual(extrapolate1(&[_]isize{ 10, 13, 16, 21, 30, 45 }), 68);
 }
 
 test partOne {
@@ -78,4 +111,18 @@ test partOne {
         \\1 3 6 10 15 21
         \\10 13 16 21 30 45
     ), 114);
+}
+
+test extrapolate2 {
+    try std.testing.expectEqual(extrapolate2(&[_]isize{ 0, 3, 6, 9, 12, 15 }), -3);
+    try std.testing.expectEqual(extrapolate2(&[_]isize{ 1, 3, 6, 10, 15, 21 }), 0);
+    try std.testing.expectEqual(extrapolate2(&[_]isize{ 10, 13, 16, 21, 30, 45 }), 5);
+}
+
+test partTwo {
+    try std.testing.expectEqual(try partTwo(
+        \\0 3 6 9 12 15
+        \\1 3 6 10 15 21
+        \\10 13 16 21 30 45
+    ), 2);
 }
